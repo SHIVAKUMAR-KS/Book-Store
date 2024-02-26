1bb6993458205b7f6fa3a2bde1fdbfbc2aa5b254
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-02-26 19:57:10 +0530

    updated backend

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,18 @@ const app =express();
 //Middleware for parsing request body
 app.use(express.json());
 
+//Middleware fro handling CORS policy
+//option1 :Allow all origin with default of cors(*)
+app.use(cors());
+//Option 2:
+app.use(
+    cors({
+        origin:'https://localhost:3000',
+        methods:['GET','POST','PUT','DELETE'],
+        allowedHeaders:['Content-Type'],
+    })
+);
+
 app.get('/',(req,res) => {
     return res.status(234).send('welcome');
 });
