5839fca98006096ba08154227a0f7dae5935c3c2
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-03-13 14:15:40 +0530

    Now in working mode both frontend and backend

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,7 @@
-export const PORT=3000;
+export const PORT = 5555;
 
-export const mongoDBURL ="mongodb+srv://shiva:a99jr8erSMuPbvSv@cluster0.n7eusza.mongodb.net/Book-store"
+export const mongoDBURL =
+  'mongodb+srv://shiva:a99jr8erSMuPbvSv@cluster0.n7eusza.mongodb.net/Book';
 
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,41 @@
-import express, { request } from "express";
-import { PORT,mongoDBURL } from "./config.js";
-import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
-import bookRoute from './routes/bookRoute.js';
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
 
-const app =express();
+const app = express();
 
-//Middleware for parsing request body
+// Middleware for parsing request body
 app.use(express.json());
 
-//Middleware fro handling CORS policy
-//option1 :Allow all origin with default of cors(*)
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
 app.use(cors());
-//Option 2:
+// Option 2: Allow Custom Origins
 // app.use(
-//     cors({
-//         origin:'https://localhost:3000',
-//         methods:['GET','POST','PUT','DELETE'],
-//         allowedHeaders:['Content-Type'],
-//     })
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
 // );
 
-app.get('/',(req,res) => {
-    return res.status(234).send('welcome');
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
-app.use('/books',bookRoute);
-
-
+app.use('/books', booksRoute);
 
 mongoose
-.connect(mongoDBURL)
-.then(() => {
-    console.log('App is listening to DATABASE');
-    app.listen(PORT,() =>{
-    console.log(`App is listening to port:${PORT}`);
-})
-})
-.catch((error) => {
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
